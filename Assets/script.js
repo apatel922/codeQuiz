2be5eea0a7428cea7756efea19557dcab772ca46
apatel922 <59633697+apatel922@users.noreply.github.com>
2020-02-16 12:25:10 -0500

    Folders set up & question list added to js file

---
+++ Assets/script.js
@@ -0,0 +1,60 @@
+
+/// Question List
+var triviaList = [
+    { /// 0
+        title: "How many bones does an adult human have?",
+        choices: ["198", "206", "284", "302"],
+        answer: "206"
+    },
+    { /// 1
+        title: "What is the national flower of Wales",
+        choices: ["Daffodil", "Woody Nightshade", "Daisy", "Foxglove"],
+        answer: "Daffodil"
+    },
+    { /// 2
+        title: "Who invented the bikini?",
+        choices: ["Len Ferd", "Luis Heard", "Chewie Beard", "Louis Reard"],
+        answer: "Louis Reard"
+    },
+    { /// 3
+        title: "When did the French Revolution end?",
+        choices: ["1811", "1804", "1799", "1791"],
+        answer: "1799"
+    },
+    { /// 4
+        title: "Who wrote the poem Paradise Lost?",
+        choices: ["Walt Whitman", "Percy Bysshe Shelley", "John Milton", "Alfred Tennyson"],
+        answer: "1799"
+    },
+    { /// 5
+        title: "Who created the animation for Monty Python?",
+        choices: ["John Cleese", "Terry Gilliam", "Graham Chapman", "Eric Idle"],
+        answer: "Terry Gilliam"
+    },
+    { /// 6
+        title: "Whereabouts in London is the Stock Exchange?",
+        choices: ["Tiananmen Square", "Old Town Square", "St. Mark's Square", "Paternoster Square"],
+        answer: "Paternoster Square"
+    },
+    { /// 7
+        title: "Whereabouts in London is the Stock Exchange?",
+        choices: ["Tiananmen Square", "Old Town Square", "St. Mark's Square", "Paternoster Square"],
+        answer: "Paternoster Square"
+    },
+    { /// 8
+        title: "Name the Corsican who captured Toulon and who sold Louisiana to America.",
+        choices: ["Charles Maurice De Talleyrand", "Napoleon Bonaparte", "Admiral Horatio Nelson", "Gebhard Blucher"],
+        answer: "Napoleon Bonaparte"
+    },
+    { /// 9
+        title: "Where on the human body is the zygomatic bone found?",
+        choices: ["Face", "Knee", "Lower Back", "Wrist"],
+        answer: "Face"
+    },
+    { /// 10
+        title: "Which famous novel featured Jo, Meg, Beth and Amy March?",
+        choices: ["Land of Love and Drowning", "The Paris Wife", "Of Mice and Men", "Little Women"],
+        answer: "Little Women"
+    },
+    ///etc.
+  ];
