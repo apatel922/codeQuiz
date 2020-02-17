3b5c4b111b78b9ee2563a662dfb85e879b9dde52
apatel922 <59633697+apatel922@users.noreply.github.com>
2020-02-17 01:05:12 -0500

    Progress Bar is Live!

--- Assets/JS/triviaSrc.js
+++ Assets/JS/triviaSrc.js
@@ -2,77 +2,77 @@
 // Question List
 var trivi1 = [
     { /// 0
-        title: "How many bones does an adult human have?",
+        title: ["How many bones does an adult human have?"],
         choices: ["198", "206", "284", "302"],
-        answer: "206"
+        answer: ["206"]
     },
     { /// 1
-        title: "What is the national flower of Wales",
+        title: ["What is the national flower of Wales?"],
         choices: ["Daffodil", "Woody Nightshade", "Daisy", "Foxglove"],
-        answer: "Daffodil"
+        answer: ["Daffodil"]
     },
     { /// 2
-        title: "Who invented the bikini?",
+        title: ["Who invented the bikini?"],
         choices: ["Len Ferd", "Luis Heard", "Chewie Beard", "Louis Reard"],
-        answer: "Louis Reard"
+        answer: ["Louis Reard"]
     },
     { /// 3
-        title: "When did the French Revolution end?",
+        title: ["When did the French Revolution end?"],
         choices: ["1811", "1804", "1799", "1791"],
-        answer: "1799"
+        answer: ["1799"]
     },
     { /// 4
-        title: "Who wrote the poem Paradise Lost?",
+        title: ["]Who wrote the poem Paradise Lost?"],
         choices: ["Walt Whitman", "Percy Bysshe Shelley", "John Milton", "Alfred Tennyson"],
-        answer: "1799"
+        answer: ["1799"]
     },
     { /// 5
-        title: "Who created the animation for Monty Python?",
+        title: ["Who created the animation for Monty Python?"],
         choices: ["John Cleese", "Terry Gilliam", "Graham Chapman", "Eric Idle"],
-        answer: "Terry Gilliam"
+        answer: ["Terry Gilliam"]
     },
     { /// 6
-        title: "Whereabouts in London is the Stock Exchange?",
+        title: ["Whereabouts in London is the Stock Exchange?"],
         choices: ["Tiananmen Square", "Old Town Square", "St. Mark's Square", "Paternoster Square"],
-        answer: "Paternoster Square"
+        answer: ["Paternoster Square"]
     },
     { /// 7
-        title: "Whereabouts in London is the Stock Exchange?",
+        title: ["Whereabouts in London is the Stock Exchange?"],
         choices: ["Tiananmen Square", "Old Town Square", "St. Mark's Square", "Paternoster Square"],
-        answer: "Paternoster Square"
+        answer: ["Paternoster Square"]
     },
     { /// 8
-        title: "Name the Corsican who captured Toulon and who sold Louisiana to America.",
+        title: ["Name the Corsican who captured Toulon and who sold Louisiana to America."],
         choices: ["Charles Maurice De Talleyrand", "Napoleon Bonaparte", "Admiral Horatio Nelson", "Gebhard Blucher"],
-        answer: "Napoleon Bonaparte"
+        answer: ["Napoleon Bonaparte"]
     },
     { /// 9
-        title: "Where on the human body is the zygomatic bone found?",
+        title: ["Where on the human body is the zygomatic bone found?"],
         choices: ["Face", "Knee", "Lower Back", "Wrist"],
-        answer: "Face"
+        answer: ["Face"]
     },
     { /// 10
-        title: "Which famous novel featured Jo, Meg, Beth and Amy March?",
+        title: ["Which famous novel featured Jo, Meg, Beth and Amy March?"],
         choices: ["Land of Love and Drowning", "The Paris Wife", "Of Mice and Men", "Little Women"],
-        answer: "Little Women"
+        answer: ["Little Women"]
     },
     ///etc.
 ];
 
-// Setting up Globals
-var trivQuestion = document.querySelector("#trivQuestion");
-var a0 = document.querySelector("#a0");
-var a1 = document.querySelector("#a1");
-var a2 = document.querySelector("#a2");
-var a3 = document.querySelector("#a3");
-var seconds = document.querySelector("#seconds");
+// Setting Up Globals
+var triviUQuest = document.querySelector("#trivQuestion");
+var answer0 = document.querySelector("#a0");
+var answer1 = document.querySelector("#a1");
+var answer2 = document.querySelector("#a2");
+var answer3 = document.querySelector("#a3");
+var secondsTimer = document.querySelector("#seconds");
+var pbar = document.querySelector("#progBar");
 // Confirming Globals
-console.log(trivQuestion, a0, a1, a2, a3, seconds);
-
-
+console.log(triviUQuest, answer0, answer1, answer2, answer3, secondsTimer, pbar);
 
 // Timer based on quiz length (10 sec per question)
 var timerStartLength = (trivi1.length) * 10;
+
 console.log(trivi1.length + " questions total.");
 console.log("10 seconds per question.");
 console.log("----------");
@@ -81,7 +81,6 @@ console.log("We started at the bottom...");
 // Timer fxn
 var startTimer = setInterval(function() {
     document.querySelector("#seconds").textContent = timerStartLength;
-    document.querySelector("#seconds").style.color = "#aaaaaa";
     timerStartLength--;
     // "Time's Up!" message
     if (timerStartLength < 0) {
@@ -92,12 +91,45 @@ var startTimer = setInterval(function() {
     }
 }, 1000);
 
+console.log(timerStartLength);
+// ^ Returned 110 (we will count up to this x % per sec)
+
+// Progress Bar fxn
+function update() { 
+    var width = 0; 
+    var identity = setInterval(countUp, (timerStartLength * 10)); 
+      
+    function countUp() { 
+        if (width >= 100) { 
+            clearInterval(identity); 
+        } else { 
+            width++;  
+            pbar.style.width = width + '%';  
+        }; 
+    }; 
+}; 
+
+// Progress Bar runs onLoad
+update();
+
+
 
 var prevQIndex = trivi1.length - 1;
 var loopingQIndex = 0;
 
 // Generate Questions fxn
-function generateQandAs () {
-    var x = trivi1[loopingQIndex];
-
+function generateQnA () {
+    var questNum = trivi1[loopingQIndex];
+    triviUQuest.textContent = questNum.title[0];
+    answer0.textContent = questNum.choices[0];
+    answer1.textContent = questNum.choices[1];
+    answer2.textContent = questNum.choices[2];
+    answer3.textContent = questNum.choices[3];
 };
+
+console.log(generateQnA);
+generateQnA();
+
+
+
+
